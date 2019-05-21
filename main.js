764484bfb5b54e2be41b35103f2662fc2d57a0eb
david082321 <7046617+david082321@users.noreply.github.com>
2019-05-21 12:22:35 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // ==UserScript==
-// @name         GitHub 汉化插件
-// @description  汉化 GitHub 界面的部分菜单及内容。
-// @copyright    2016, 楼教主 (http://www.52cik.com/)
+// @name         GitHub 漢化外掛程式
+// @description  漢化 GitHub 介面的部分選單及內容。
+// @copyright    2016, 樓教主 (http://www.52cik.com/)
 // @icon         https://assets-cdn.github.com/pinned-octocat.svg
 // @version      1.6.4
-// @author       楼教主
+// @author       樓教主
 // @license      MIT
 // @homepageURL  https://github.com/52cik/github-hans
 // @match        http://*.github.com/*
 // @match        https://*.github.com/*
-// @require      https://52cik.github.io/github-hans/locals.js?v1.6.4
+// @require      https://raw.githubusercontent.com/david082321/github-hans/gh-pages/locals.js
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
@@ -19,22 +19,22 @@
 
     var lang = 'zh'; // 中文
 
-    // 2016-04-18 github 将 jquery 以 amd 加载，不暴露到全局了。
+    // 2016-04-18 github 將 jquery 以 amd 載入，不暴露到全域了。
     // var $ = require('github/jquery')['default'];
 
-    // 要翻译的页面
+    // 要翻譯的頁面
     var page = getPage();
 
-    transTitle(); // 页面标题翻译
-    timeElement(); // 时间节点翻译
-    // setTimeout(contributions, 100); // 贡献日历翻译 (日历是内嵌或ajax的, 所以基于回调事件处理)
-    walk(document.body); // 立即翻译页面
+    transTitle(); // 頁面標題翻譯
+    timeElement(); // 時間節點翻譯
+    // setTimeout(contributions, 100); // 貢獻日曆翻譯 (日曆是內嵌或ajax的, 所以基於回調事件處理)
+    walk(document.body); // 立即翻譯頁面
 
-    // 2017-03-19 github 屏蔽 require 改为 Promise 形式的 ghImport
+    // 2017-03-19 github 封鎖 require 改為 Promise 形式的 ghImport
     define('github-hans-ajax', ['./jquery'], function($) {
         $(document).ajaxComplete(function () {
             transTitle();
-            walk(document.body); // ajax 请求后再次翻译页面
+            walk(document.body); // ajax 請求後再次翻譯頁面
         });
     });
     ghImport('github-hans-ajax')['catch'](function(e) {
@@ -42,45 +42,45 @@
     });
 
     /**
-     * 遍历节点
+     * 遍歷節點
      *
-     * @param {Element} node 节点
+     * @param {Element} node 節點
      */
     function walk(node) {
         var nodes = node.childNodes;
 
         for (var i = 0, len = nodes.length; i < len; i++) {
             var el = nodes[i];
-            // todo 1. 修复多属性翻译问题; 2. 添加事件翻译, 如论预览信息;
+            // todo 1. 修復多屬性翻譯問題; 2. 添加事件翻譯, 如論預覽訊息;
 
-            if (el.nodeType === Node.ELEMENT_NODE) { // 元素节点处理
+            if (el.nodeType === Node.ELEMENT_NODE) { // 元素節點處理
 
-                // 元素节点属性翻译
-                if (el.tagName === 'INPUT' || el.tagName === 'TEXTAREA') { // 输入框 按钮 文本域
+                // 元素節點屬性翻譯
+                if (el.tagName === 'INPUT' || el.tagName === 'TEXTAREA') { // 輸入框 按鈕 文字域
                     if (el.type === 'button' || el.type === 'submit') {
                         transElement(el, 'value');
                     } else {
                         transElement(el, 'placeholder');
                     }
-                } else if (el.hasAttribute('aria-label')) { // 带提示的元素，类似 tooltip 效果的
+                } else if (el.hasAttribute('aria-label')) { // 帶提示的元素，類似 tooltip 效果的
                     transElement(el, 'aria-label', true);
 
-                    if (el.hasAttribute('data-copied-hint')) { // 复制成功提示
+                    if (el.hasAttribute('data-copied-hint')) { // 複製成功提示
                         transElement(el.dataset, 'copiedHint');
                     }
-                } else if (el.tagName === 'OPTGROUP') { // 翻译 <optgroup> 的 label 属性
+                } else if (el.tagName === 'OPTGROUP') { // 翻譯 <optgroup> 的 label 屬性
                     transElement(el, 'label');
                 }
 
-                if (el.hasAttribute('data-disable-with')) { // 按钮等待提示
+                if (el.hasAttribute('data-disable-with')) { // 按鈕等待提示
                     transElement(el.dataset, 'disableWith');
                 }
 
-                // 跳过 readme, 文件列表, 代码显示
+                // 跳過 readme, 文件列表, 程式碼顯示
                 if (el.id !== 'readme' && !I18N.conf.reIgnore.test(el.className)) {
-                    walk(el); // 遍历子节点
+                    walk(el); // 遍歷子節點
                 }
-            } else if (el.nodeType === Node.TEXT_NODE) { // 文本节点翻译
+            } else if (el.nodeType === Node.TEXT_NODE) { // 文字節點翻譯
                 transElement(el, 'data');
             }
 
@@ -88,30 +88,30 @@
     }
 
     /**
-     * 获取翻译页面
+     * 獲取翻譯頁面
      */
     function getPage() {
         // 先匹配 body 的 class
         var page = document.body.className.match(I18N.conf.rePageClass);
 
-        if (!page) { // 扩展 url 匹配
+        if (!page) { // 擴展 url 匹配
             page = location.href.match(I18N.conf.rePageUrl);
         }
 
-        if (!page) { // 扩展 pathname 匹配
+        if (!page) { // 擴展 pathname 匹配
             page = location.pathname.match(I18N.conf.rePagePath);
         }
 
-        return page ? page[1] || 'homepage' : false; // 取页面 key
+        return page ? page[1] || 'homepage' : false; // 取頁面 key
     }
 
     /**
-     * 翻译页面标题
+     * 翻譯頁面標題
      */
     function transTitle() {
         var title = translate(document.title, 'title');
 
-        if (title === false) { // 无翻译则退出
+        if (title === false) { // 無翻譯則退出
             return false;
         }
 
@@ -120,28 +120,28 @@
 
 
     /**
-     * 翻译节点对应属性内容
+     * 翻譯節點對應屬性內容
      *
-     * @param {object} el 对象
-     * @param {string} field 属性字段
-     * @param {boolean} isAttr 是否是 attr 属性
+     * @param {object} el 物件
+     * @param {string} field 屬性欄位
+     * @param {boolean} isAttr 是否是 attr 屬性
      *
      * @returns {boolean}
      */
     function transElement(el, field, isAttr) {
-        var transText = false; // 翻译后的文本
+        var transText = false; // 翻譯後的文字
 
-        if (isAttr === undefined) { // 非属性翻译
+        if (isAttr === undefined) { // 非屬性翻譯
             transText = translate(el[field], page);
         } else {
             transText = translate(el.getAttribute(field), page);
         }
 
-        if (transText === false) { // 无翻译则退出
+        if (transText === false) { // 無翻譯則退出
             return false;
         }
 
-        // 替换翻译后的内容
+        // 取代翻譯後的內容
         if (isAttr === undefined) {
             el[field] = transText;
         } else {
@@ -151,64 +151,64 @@
 
 
     /**
-     * 翻译文本
+     * 翻譯文字
      *
-     * @param {string} text 待翻译字符串
-     * @param {string} page 页面字段
+     * @param {string} text 待翻譯字串
+     * @param {string} page 頁面欄位
      *
      * @returns {string|boolean}
      */
-    function translate(text, page) { // 翻译
+    function translate(text, page) { // 翻譯
         var str;
         var _key = text.trim(); // 去除首尾空格的 key
         var _key_neat = _key
-            .replace(/\xa0/g, ' ') // 替换 &nbsp; 空格导致的 bug
-            .replace(/\s{2,}/g, ' '); // 去除多余换行空格等字符，(试验测试阶段，有问题再恢复)
+            .replace(/\xa0/g, ' ') // 取代 &nbsp; 空格導致的 bug
+            .replace(/\s{2,}/g, ' '); // 去除多餘換行空格等字元，(試驗測試階段，有問題再復原)
 
         if (_key_neat === '') {
             return false;
-        } // 内容为空不翻译
+        } // 內容為空不翻譯
 
-        str = transPage('pubilc', _key_neat); // 公共翻译
+        str = transPage('pubilc', _key_neat); // 公共翻譯
 
-        if (str !== false && str !== _key_neat) { // 公共翻译完成
-            str = transPage('pubilc', str) || str;  // 二次公共翻译（为了弥补正则部分翻译的情况）
-            return text.replace(_key, str);  // 替换原字符，保留空白部分
+        if (str !== false && str !== _key_neat) { // 公共翻譯完成
+            str = transPage('pubilc', str) || str;  // 二次公共翻譯（為了彌補正則部分翻譯的情況）
+            return text.replace(_key, str);  // 取代原字元，保留空白部分
         }
 
         if (page === false) {
             return false;
-        } // 未知页面不翻译
+        } // 未知頁面不翻譯
 
-        str = transPage(page, _key_neat); // 翻译已知页面
+        str = transPage(page, _key_neat); // 翻譯已知頁面
         if (str === false || str === '') {
             return false;
-        } // 未知内容不翻译
+        } // 未知內容不翻譯
 
-        str = transPage('pubilc', str) || str; // 二次公共翻译（为了弥补正则部分翻译的情况）
-        return text.replace(_key, str); // 替换原字符，保留空白部分
+        str = transPage('pubilc', str) || str; // 二次公共翻譯（為了彌補正則部分翻譯的情況）
+        return text.replace(_key, str); // 取代原字元，保留空白部分
     }
 
 
     /**
-     * 翻译页面内容
+     * 翻譯頁面內容
      *
-     * @param {string} page 页面
-     * @param {string} key 待翻译内容
+     * @param {string} page 頁面
+     * @param {string} key 待翻譯內容
      *
      * @returns {string|boolean}
      */
     function transPage(page, key) {
-        var str; // 翻译结果
-        var res; // 正则数组
+        var str; // 翻譯結果
+        var res; // 正則陣列
 
-        // 静态翻译
+        // 靜態翻譯
         str = I18N[lang][page]['static'][key];
         if (str) {
             return str;
         }
 
-        // 正则翻译
+        // 正則翻譯
         res = I18N[lang][page].regexp;
         if (res) {
             for (var i = 0, len = res.length; i < len; i++) {
@@ -219,15 +219,15 @@
             }
         }
 
-        return false; // 没有翻译条目
+        return false; // 沒有翻譯條目
     }
 
 
     /**
-     * 时间节点翻译
+     * 時間節點翻譯
      */
     function timeElement() {
-        if (!window.RelativeTimeElement) { // 防止报错
+        if (!window.RelativeTimeElement) { // 防止報錯
             return;
         }
 
@@ -235,12 +235,12 @@
         var TimeAgoElement$getFormattedDate = TimeAgoElement.prototype.getFormattedDate;
         // var LocalTimeElement$getFormattedDate = LocalTimeElement.prototype.getFormattedDate;
 
-        var RelativeTime = function (str, el) { // 相对时间解析
+        var RelativeTime = function (str, el) { // 相對時間解析
             if (/^on ([\w ]+)$/.test(str)) {
-                return '于 ' + el.title.replace(/ .+$/, '');
+                return '於 ' + el.title.replace(/ .+$/, '');
             }
 
-            // 使用字典公共翻译的第二个正则翻译相对时间
+            // 使用字典公共翻譯的第二個正則翻譯相對時間
             var time_ago = I18N[lang].pubilc.regexp[1];
             return str.replace(time_ago[0], time_ago[1]);
         };
@@ -259,11 +259,11 @@
             return this.title.replace(/ .+$/, '');
         };
 
-        // 遍历 time 元素进行翻译
-        // 2016-04-16 github 改版，不再用 time 标签了。
+        // 遍歷 time 元素進行翻譯
+        // 2016-04-16 github 改版，不再用 time 標籤了。
         var times = document.querySelectorAll('time, relative-time, time-ago, local-time');
         Array.prototype.forEach.call(times, function (el) {
-            if (el.getFormattedDate) { // 跳过未注册的 time 元素
+            if (el.getFormattedDate) { // 跳過未註冊的 time 元素
                 el.textContent = el.getFormattedDate();
             }
         });
@@ -271,31 +271,31 @@
 
 
     /**
-     * 贡献日历 基于事件翻译
+     * 貢獻日曆 基於事件翻譯
      */
     function contributions() {
         var tip = document.getElementsByClassName('svg-tip-one-line');
 
-        // 等待 IncludeFragmentElement 元素加载完毕后绑定事件
+        // 等待 IncludeFragmentElement 元素載入完畢後綁定事件
         // var observe = require('github/observe').observe;
 
         define('github/hans-contributions', ['./observe'], function (observe) {
             observe(".js-calendar-graph-svg", function () {
-                setTimeout(function () { // 延时绑定 mouseover 事件，否则没法翻译
+                setTimeout(function () { // 延時綁定 mouseover 事件，否則沒辦法翻譯
                     var $calendar = $('.js-calendar-graph');
-                    walk($calendar[0]); // 翻译日历部分
+                    walk($calendar[0]); // 翻譯日曆部分
 
                     $calendar.on('mouseover', '.day', function () {
-                        if (tip.length === 0) { // 没有 tip 元素时退出防止报错
+                        if (tip.length === 0) { // 沒有 tip 元素時退出防止報錯
                             return true;
                         }
 
-                        var data = $(this).data(); // 获取节点上的 data
+                        var data = $(this).data(); // 獲取節點上的 data
                         var $tip = $(tip[0]);
 
-                        $tip.html(data.count + ' 次贡献 ' + data.date);
+                        $tip.html(data.count + ' 次貢獻 ' + data.date);
 
-                        var rect = this.getBoundingClientRect(); // 获取元素位置
+                        var rect = this.getBoundingClientRect(); // 獲取元素位置
                         var left = rect.left + window.pageXOffset - tip[0].offsetWidth / 2 + 5.5;
 
                         $tip.css('left', left);
